c19408e96529854d3f2e944317e601725b9ce107
Brandon Michaud <brandondmichaud@ou.edu>
2022-03-09 23:33:11 -0600

    Add FIREDAM data

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Fire damage and proximity to a fire station
+#'
+#' A dataset with two quantitative variables
+#' Interest is in the relationship between proximity to a fire station and damage
+#'
+#' @format A dataframe with 15 rows and 2 variables:
+#' \describe{
+#' \item{DISTANCE}{Closest distance in miles to a fire station}
+#' \item{DAMAGE}{Damage in thousands of dollars caused by fire}
+#' }
+#'
+#' @source Statistics for Engineering and the Sciences, 6th Edition
+"fire"
